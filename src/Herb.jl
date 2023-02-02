a41110bea4b20e33c30450f61937e06cc465f8cb
Jaap de Jong <jaapdejong15@gmail.com>
2023-02-02 14:11:09 +0100

    Add exports to main Herb.jl file

--- src/Herb.jl
+++ src/Herb.jl
@@ -2,13 +2,19 @@ module Herb
 
 
 include("../Grammar.jl/src/Grammars.jl")
-include("../Evaluation.jl/src/Evaluation.jl")
 include("../Data.jl/src/Data.jl")
+include("../Evaluation.jl/src/Evaluation.jl")
 include("../Search.jl/src/Search.jl")
 
 using .Grammars
+using .Data
 using .Evaluation
 using .Search
-using .Data
+
+export 
+    Grammars,
+    Evaluation,
+    Data,
+    Search
 
 end # module
