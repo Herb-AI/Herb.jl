bf66034c9a018741c9f94590f772b7c4a9786afc
Jaap de Jong <jaapdejong15@gmail.com>
2023-02-16 10:41:44 +0100

    Add specification extraction module

--- src/Herb.jl
+++ src/Herb.jl
@@ -5,16 +5,19 @@ include("../Grammar.jl/src/Grammars.jl")
 include("../Data.jl/src/Data.jl")
 include("../Evaluation.jl/src/Evaluation.jl")
 include("../Search.jl/src/Search.jl")
+include("../SpecificationExtraction.jl/src/SpecificationExtraction.jl")
 
 using .Grammars
 using .Data
 using .Evaluation
 using .Search
+using .SpecificationExtraction
 
 export 
     Grammars,
     Evaluation,
     Data,
-    Search
+    Search,
+    SpecificationExtraction
 
 end # module
