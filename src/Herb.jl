ead5bcec98a9d7466559c8bc5977f3583af21257
Jaap de Jong <jaapdejong15@gmail.com>
2023-02-22 17:01:26 +0100

    Connect constraints module and tests

--- src/Herb.jl
+++ src/Herb.jl
@@ -1,20 +1,22 @@
 module Herb
 
-
 include("../Grammar.jl/src/Grammars.jl")
+include("../Constraints.jl/src/Constraints.jl")
 include("../Data.jl/src/Data.jl")
 include("../Evaluation.jl/src/Evaluation.jl")
 include("../Search.jl/src/Search.jl")
 
 using .Grammars
+using .Constraints
 using .Data
 using .Evaluation
 using .Search
 
 export 
     Grammars,
-    Evaluation,
+    Constraints,
     Data,
+    Evaluation,
     Search
 
 end # module
--- test/herb_test.jl
+++ test/herb_test.jl
@@ -3,12 +3,18 @@ module HerbTest
 include("../src/Herb.jl")
 
 using .Herb.Grammars
+using .Herb.Constraints
 using .Herb.Evaluation
 using .Herb.Search
 using .Herb.Data
 using Test
 
-# include("../Grammar.jl/test/test_cfg.jl")
-include("../Search.jl/test/test_iterators.jl")
 
+@testset verbose=true "Herb" begin
+    include("../Grammar.jl/test/test_cfg.jl")
+    include("../Grammar.jl/test/test_csg.jl")
+    include("../Constraints.jl/test/test_propagators.jl")
+    include("../Search.jl/test/test_iterators.jl")
 end
+
+end # module
