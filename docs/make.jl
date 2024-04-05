dc9fe326fab670d02c5ffbd8c83209eeee9e205a
Tilman Hinnerichs <tilman@hinnerichs.com>
2024-04-05 11:34:19 +0200

    Ignore crossref issues

--- docs/make.jl
+++ docs/make.jl
@@ -37,7 +37,7 @@ makedocs(
     format = Documenter.HTML(        
         sidebar_sitename = false
     ),
-    warnonly = [:missing_docs]
+    warnonly = [:missing_docs, :cross_reference]
 )
 
 deploydocs(;
