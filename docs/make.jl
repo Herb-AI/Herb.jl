8985a95f85724a86eb8f39315c3a4ed9d66c43a9
Pamela Wochner <p.wochner@tudelft.nl>
2024-07-09 12:25:37 +0200

    Embed html of interpreter tutorial in md file. Increase html size.

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Herb
 
 using HerbConstraints
 using HerbSearch
-using HerbGrammar 
+using HerbGrammar
 using HerbInterpret
 using HerbCore
 using HerbSpecification
@@ -13,19 +13,19 @@ makedocs(
     modules=[HerbConstraints, HerbSearch, HerbGrammar, HerbSpecification, HerbInterpret, HerbCore],
     authors="PONYs",
     sitename="Herb.jl",
-    pages = [
+    pages=[
         "Basics" => [
             "index.md",
             "install.md",
             "get_started.md",
             "concepts.md"
-           ],
+        ],
         "Tutorials" => [
             "A more verbose getting started with Herb.jl" => "tutorials/getting_started_with_herb.md",
             "Defining Grammars in Herb.jl" => "tutorials/defining_grammars.md",
             "Advanced Search Procedures" => "tutorials/advanced_search.md",
             "Getting started with Constraints" => "tutorials/getting_started_with_constraints.md",
-            "Working with custom interpreters" => "tutorials/working_with_interpreters.html"
+            "Working with custom interpreters" => "tutorials/working_with_interpreters.md"
         ],
         "Sub-Modules" => [
             "HerbCore.jl" => "HerbCore/index.md",
@@ -34,12 +34,14 @@ makedocs(
             "HerbInterpret.jl" => "HerbInterpret/index.md",
             "HerbConstraints.jl" => "HerbConstraints/index.md",
             "HerbSearch.jl" => "HerbSearch/index.md",
-       ],
+        ],
     ],
-    format = Documenter.HTML(        
-        sidebar_sitename = false
+    format=Documenter.HTML(
+        sidebar_sitename=false,
+        size_threshold=512000, # Set to a higher value
+        size_threshold_warn=102400
     ),
-    warnonly = [:missing_docs, :cross_references, :doctest]
+    warnonly=[:missing_docs, :cross_references, :doctest]
 )
 
 deploydocs(;
