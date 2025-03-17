f09096c8f25e6ff1d9c1a1a3ed5b41e959bfce01
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-03-17 12:24:23 +0100

    Fix use of $ outside of a quote

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -16,13 +16,14 @@ g = @csgrammar begin
 end
 
 max_depth = 5
+max_size = typemax(Int)
 
 if PACKAGE_VERSION == v"0.1.0"
     SUITE["search"]["BFS over small addition grammar"] = @benchmarkable(
-        count_expressions($g, $max_depth, $(typemax(Int)), :Int)
+        count_expressions($g, $max_depth, $max_size, :Int)
     )
 else
-    iter = BFSIterator(g, :Int; max_depth=$max_depth)
+    iter = BFSIterator(g, :Int; max_depth=max_depth)
     SUITE["search"]["BFS over small addition grammar"] = @benchmarkable(
         collect($iter)
     )
