cd09973ff94dcc459077493da5a18d4c43a0069f
Pamela Wochner <p.wochner@tudelft.nl>
2025-07-16 16:57:15 +0100

    Explicit import for test environment

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,16 @@ using Aqua
 using Herb
 using Test
 include(joinpath(@__DIR__, "..", "docs", "build_notebooks.jl"))
+import Pkg
 
 @testset "Herb" begin
-   @testset "Aqua.jl" begin
-      Aqua.test_all(Herb)
-   end
+    @testset "Aqua.jl" begin
+        Aqua.test_all(Herb)
+    end
 
-   @testset "Build notebooks" begin
-      tutorials_dir = joinpath(dirname(@__DIR__), "docs", "src", "tutorials")
-      # Smoke test to make sure building the notebooks doesn't error
-      @test build(tutorials_dir) isa Any
-   end
+    @testset "Build notebooks" begin
+        tutorials_dir = joinpath(dirname(@__DIR__), "docs", "src", "tutorials")
+        # Smoke test to make sure building the notebooks doesn't error
+        @test build(tutorials_dir) isa Any
+    end
 end
