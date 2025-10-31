9987df0b9f248f54c42781b2156827e12c035219
Maarten Flippo <maartenflippo@outlook.com>
2025-10-31 12:47:53 +0100

    docs: Expand the getting started guide
    This comment elaborates on the contents of the getting started guide. It
    changes the following:
    
    - Add links to the documentation of every used Herb
    function/module/type.
    - Explain the purpose of every expression.
    - Clarify how symbols in the grammar relate to Julia symbols.

--- docs/src/tutorials/basic_getting_started.jl
+++ docs/src/tutorials/basic_getting_started.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.13
+# v0.20.20
 
 using Markdown
 using InteractiveUtils
@@ -26,10 +26,14 @@ To begin, we need to import `Herb`.
 # ╔═╡ 27e843c5-df97-4ede-9294-88cb3b5df748
 md"
 To define a program synthesis problem, we need a grammar and specification. 
+- The grammar defines the shape of the syntax tree. 
+- The specification describes the behavior of the desired program. In this guide, the specification is a collection of input/output examples, but [`HerbSpecification`](https://herb-ai.github.io/Herb.jl/dev/HerbSpecification/) defines other forms of specification as well.
 
-First, a grammar can be constructed using the `@csgrammar` macro included in `HerbGrammar`. 
+Having defined a problem, we use [`HerbSearch`](https://herb-ai.github.io/Herb.jl/dev/HerbSearch/) to find syntax trees that match the specification. These trees can be executed using [`HerbInterpret`](https://herb-ai.github.io/Herb.jl/dev/HerbInterpret/).
 
-Here, we describe a simple integer arithmetic example, that can add and multiply an input variable `x` or the integers `1,2`, using
+## The Grammar
+
+We will start by creating a grammar using the [`@csgrammar`](https://herb-ai.github.io/Herb.jl/dev/HerbGrammar/#HerbGrammar.@csgrammar-Tuple{Any}) macro. Here, we describe a simple integer arithmetic example, that can add and multiply an input variable `x` or the integers `1,2`.
 
 "
 
@@ -43,35 +47,74 @@ end
 
 # ╔═╡ b55c696a-a421-462a-abfd-b8f2c652d4f0
 md"
-Second, the problem specification can be provided using e.g. input/output examples using `HerbSpecification`. Inputs are provided as a `Dict` assigning values to variables, and outputs as arbitrary values. The problem itself is then a list of `IOExample`s using
+!!! note
+
+    The symbol `x` in the grammar corresponds to a variable in the arithmetic expression. It can have any name in the grammar, as long as the name does not collide with already defined symbols.
+
+## The Specification
+
+Separate from the grammar we specify the behavior of the program using input/output examples. Inputs are provided as a [`Dict`](https://docs.julialang.org/en/v1/base/collections/#Base.Dict), where the keys are values for the variable `x` and the values are the outputs of the program. The problem itself is then a list of [`IOExample`](https://herb-ai.github.io/Herb.jl/dev/HerbSpecification/#HerbSpecification.IOExample)s.
 "
 
 # ╔═╡ dc8926aa-91c1-4a04-ab8d-6820edc6db2b
-problem = Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5])
+specification = [IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5]
+
+# ╔═╡ 7e1a10b5-8c24-41fb-a3aa-4ec1617d05cf
+md"
+The specification is used to construct a problem instance. It will be combined with the grammar once we start searching for programs in the next section.
+"
+
+# ╔═╡ 63af0e76-db88-432d-9fc1-11ec016c45c2
+problem = Problem(specification)
 
 # ╔═╡ 08cc1274-3ea2-4e21-891b-e4cca9281eeb
 md"
-The problem is given now, let us search for a solution with `HerbSearch`. For now, we will just use the default parameters searching for a satisfying program over the grammar, given the problem and a starting symbol using
+
+# Searching for Programs
+
+Having defined the grammar and problem, let us search for a solution with [`HerbSearch`](https://herb-ai.github.io/Herb.jl/dev/HerbSearch/). 
+
+To do so, we will iterate the space of all possible program trees in a breadth-first manner using a [`BFSIterator`](https://herb-ai.github.io/Herb.jl/dev/HerbSearch/#HerbSearch.BFSIterator). It starts with program tree consisting of a single `:Number` node, and is bounded by setting the maximum depth of the program tree to 5.
 "
 
 # ╔═╡ 2732bd85-4076-4530-99bf-9076a8811329
-iterator = BFSIterator(g, :Number, max_depth=5)
+program_tree_iterator = BFSIterator(g, :Number, max_depth=5)
 
-# ╔═╡ 01ce2ad4-ad00-41cf-afe3-757ae62ac4c6
-solution, flag = synth(problem, iterator)
+# ╔═╡ 5a4a491d-dcb0-4623-90ea-d3c5e59a5f72
+md"
+
+The problem can then be handed to the [`synth`](https://herb-ai.github.io/Herb.jl/dev/HerbSearch/#HerbSearch.synth-Tuple{Problem,%20ProgramIterator}) function to run the search. It will iterate over all program trees to find a program that satisfies the specification. When it terminates, it returns the program that satisfies the most examples, along with a flag that indicates whether the program satisfies _all_ the examples or not.
+
+"
+
+# ╔═╡ 0983097a-d592-44e3-950b-9d9dfeb30edf
+solution, flag = synth(problem, program_tree_iterator)
 
 # ╔═╡ a94e49d4-a348-4af9-8236-f18dcbe7ef5c
 md"
-There are various ways to adapt the search technique to your needs. Please have a look at the [`synth`](@ref) documentation.
+There are various ways to adapt the search technique to your needs. Look at the [`synth`](https://herb-ai.github.io/Herb.jl/dev/HerbSearch/#HerbSearch.synth-Tuple{Problem,%20ProgramIterator}) documentation for all the available options.
 
-Eventually, we want to test our solution on some other inputs using `HerbInterpret`. We transform our grammar `g` to a Julia expression with `grammar2symboltable(g)`, add our solution and the input, assigning the value `6` to the variable `x`.
+## Using the Synthesized Program
+
+Eventually, we want to test our solution on some other inputs. This means we need a way to execute the solution, for which we provide [`HerbInterpret`](https://herb-ai.github.io/Herb.jl/dev/HerbInterpret/).
+
+We transform the solution into a Julia expression with [`rulenode2expr`](https://herb-ai.github.io/Herb.jl/dev/HerbGrammar/#HerbGrammar.rulenode2expr-Tuple%7BAbstractRuleNode,%20AbstractGrammar%7D). The resulting expression can be executed using Julia's builtin [`eval`](https://docs.julialang.org/en/v1/base/base/#eval) function.
 "
 
 # ╔═╡ fb3e47bf-03f1-4eda-85ff-1897751c1d00
 program = rulenode2expr(solution, g)
 
+# ╔═╡ 76d48e3c-0347-4d49-afed-7812feee930c
+md"
+
+To set up an environment that correctly binds values to the variables in the program, we provide the [`execute_on_input`](https://herb-ai.github.io/Herb.jl/dev/HerbInterpret/#HerbInterpret.execute_on_input-Union{Tuple{T},%20Tuple{Dict{Symbol,%20Any},%20Any,%20Dict{Symbol,%20T}}}%20where%20T) function. It takes in three arguments:
+- A [`SymbolTable`](https://herb-ai.github.io/Herb.jl/dev/HerbGrammar/#HerbGrammar.SymbolTable), which is nothing more than a dictionary mapping symbols from the grammar to symbols in the Julia expression. This can be created using the [`grammar2symboltable`](https://herb-ai.github.io/Herb.jl/dev/HerbGrammar/#HerbGrammar.grammar2symboltable) function.
+- The program to execute. **Note:** This is not the solution from the search, but the Julia expression built from the solution.
+- Values for all the variables in the expression.
+"
+
 # ╔═╡ 4ce7545e-2f1c-40c3-9044-5092b66d3c3c
-output = execute_on_input(grammar2symboltable(g), program, Dict(:x => 6))  # should yield 2*6+1
+output = execute_on_input(grammar2symboltable(g), program, Dict(:x => 6))  # yields 2*6+1
 
 # ╔═╡ b049977a-71d0-4e3e-b915-626aa5909bd6
 md"
@@ -118,15 +161,19 @@ println(output)
 # ╟─0677e60e-d77c-4bda-ac8e-6ff8b21f0431
 # ╠═146ff53a-eab6-4b31-82fc-370bbdc4236f
 # ╠═0c1f68b7-a802-4d2a-b1a7-e4da11945907
-# ╟─27e843c5-df97-4ede-9294-88cb3b5df748
+# ╠═27e843c5-df97-4ede-9294-88cb3b5df748
 # ╠═eec315e4-ae65-4d97-ac9e-8b31a6d3ad0e
-# ╟─b55c696a-a421-462a-abfd-b8f2c652d4f0
+# ╠═b55c696a-a421-462a-abfd-b8f2c652d4f0
 # ╠═dc8926aa-91c1-4a04-ab8d-6820edc6db2b
-# ╟─08cc1274-3ea2-4e21-891b-e4cca9281eeb
+# ╠═7e1a10b5-8c24-41fb-a3aa-4ec1617d05cf
+# ╠═63af0e76-db88-432d-9fc1-11ec016c45c2
+# ╠═08cc1274-3ea2-4e21-891b-e4cca9281eeb
 # ╠═2732bd85-4076-4530-99bf-9076a8811329
-# ╠═01ce2ad4-ad00-41cf-afe3-757ae62ac4c6
-# ╟─a94e49d4-a348-4af9-8236-f18dcbe7ef5c
+# ╠═5a4a491d-dcb0-4623-90ea-d3c5e59a5f72
+# ╠═0983097a-d592-44e3-950b-9d9dfeb30edf
+# ╠═a94e49d4-a348-4af9-8236-f18dcbe7ef5c
 # ╠═fb3e47bf-03f1-4eda-85ff-1897751c1d00
+# ╠═76d48e3c-0347-4d49-afed-7812feee930c
 # ╠═4ce7545e-2f1c-40c3-9044-5092b66d3c3c
 # ╟─b049977a-71d0-4e3e-b915-626aa5909bd6
 # ╟─cccf503e-f36a-11ef-1697-3f13720272cf
